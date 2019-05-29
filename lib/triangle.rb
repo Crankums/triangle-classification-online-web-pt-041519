0e89f2c94dd4b7d4bd4062e09cee23e38e143e23
Craig Donavin <crdonavin@gmail.com>
2019-05-29 00:55:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,8 +7,9 @@ class Triangle
     @side_three = side_three
   end
 
-  def kind(side_one,side_two,side_three)
-    if side_one == side_two && side_two == side_three
+  def kind
+    
+    if @side_one == @side_two && @side_two == @side_three
       :equilateral
     #elsif side_one == side_two || side_two == side_three || side_one == side_three
   end
