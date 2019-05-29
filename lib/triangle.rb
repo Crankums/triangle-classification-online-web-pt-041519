884481556e78d98ce760a193703b410d68812968
Craig Donavin <crdonavin@gmail.com>
2019-05-29 02:09:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,16 +19,18 @@ class Triangle
     end
   end
 
-  # def zero?
-  #   arr = [side_one, side_two, side_three]
-  #   raise TriangleError if arr.any?{|side| side <= 0}
-  # end
+  def zero?
+    arr = [side_one, side_two, side_three]
+    raise TriangleError if arr.any?{|side| side <= 0}
+  end
 
   # def legal_triangle
     # triangle = [ (side_one + side_two > side_three),
     #               (side_two + side_three > side_one),
     #               (side_one + side_three > side_two) ]
 
-  class TriangleError < StandardError
-  end
+  
+end
+
+class TriangleError < StandardError
 end
