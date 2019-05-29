d1219dc7d0d3f76384bccfa2280dfd98d9ec88d1
Craig Donavin <crdonavin@gmail.com>
2019-05-29 02:15:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
     @side_three = side_three
   end
 
-  def kind
+  def kind()
     if (@side_one <= 0) || (@side_two <= 0) || (@side_three <= 0)
       raise TriangleError
       # if side_one == side_two && side_two == side_three
