b5fb89d12d55d03144c99b7de35cc179899eeefb
Craig Donavin <crdonavin@gmail.com>
2019-05-29 01:47:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,7 +19,7 @@ class Triangle
 
   def zero?
     arr = [side_one, side_two, side_three]
-    arr.any?{|side| side == 0}
+    raise TriangleError if arr.any?{|side| side == 0}
   end
 
   # def legal_triangle
